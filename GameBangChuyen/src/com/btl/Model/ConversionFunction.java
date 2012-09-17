41bbb50dfdf6ad7d11ac9ed800fe4c767bfcd4a3
DucNX <xuanduc987@gmail.com>
2012-09-17 22:03:29 +0700

    Convention
    Đưa về đúng code convention

--- GameBangChuyen/src/com/btl/Model/ConversionFunction.java
+++ GameBangChuyen/src/com/btl/Model/ConversionFunction.java
@@ -1,28 +1,30 @@
 package com.btl.Model;
 
 import java.awt.Point;
-import java.awt.Polygon;
 
 public class ConversionFunction {
 	/**
 	 * 
-	 * @param t : point in logic axis
+	 * @param t
+	 *            : point in logic axis
 	 * @return: point in real axis
 	 */
-	public static Point logicToReal(Point t){
-		Double uy = (double) (t.x - t.y) / Math.sqrt(3); 
-		Double ux = (double) (t.y + t.x); 
-		return new Point(ux.intValue(), uy.intValue()); 
+	public static Point logicToReal(Point t) {
+		Double uy = (double) (t.x - t.y) / Math.sqrt(3);
+		Double ux = (double) (t.y + t.x);
+		return new Point(ux.intValue(), uy.intValue());
 	}
+
 	/**
 	 * 
-	 * @param t : point in real axis
+	 * @param t
+	 *            : point in real axis
 	 * @return: point in logic axis
 	 */
-	public static Point realToLogic(Point t){
-		Double ux = (double)(Math.sqrt(3)*t.y + t.x)/2;
-		Double uy = (double)(t.x - Math.sqrt(3)*t.y)/2;
-		return new Point(ux.intValue(),uy.intValue());
+	public static Point realToLogic(Point t) {
+		Double ux = (double) (Math.sqrt(3) * t.y + t.x) / 2;
+		Double uy = (double) (t.x - Math.sqrt(3) * t.y) / 2;
+		return new Point(ux.intValue(), uy.intValue());
 	}
-  
+
 }
