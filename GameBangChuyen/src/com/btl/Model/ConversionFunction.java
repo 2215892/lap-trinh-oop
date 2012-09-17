f5d2dcef2882290b295fa76405965dabee0732a3
kihemaitien <kihemaitien@gmail.com>
2012-09-17 13:47:34 +0000

    xóa phần thừa

--- GameBangChuyen/src/com/btl/Model/ConversionFunction.java
+++ GameBangChuyen/src/com/btl/Model/ConversionFunction.java
@@ -24,12 +24,5 @@ public class ConversionFunction {
 		Double uy = (double)(t.x - Math.sqrt(3)*t.y)/2;
 		return new Point(ux.intValue(),uy.intValue());
 	}
-	/**
-	 * 
-	 * @param s diem goc cua hinh vuong can ve toa do logic
-	 * @return tu giac toa do thuc
-	 */
-	
-	
-
+  
 }
