469ba8155c7cf11c6792568292d302e3b38efd53
kihemaitien <kihemaitien@gmail.com>
2012-09-17 13:49:22 +0000

    terminal

---
+++ GameBangChuyen/src/com/btl/Model/Terminal.java
@@ -0,0 +1,15 @@
+package com.btl.Model;
+
+import java.awt.Point;
+
+public class Terminal {
+	Point position;
+	
+	public Terminal( Point p){
+		position = p;
+	}
+	
+	public Point getPosition(){
+		return position;
+	}
+}
