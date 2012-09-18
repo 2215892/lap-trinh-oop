68995e49e4ef7632831b417654e2ebe0af2dab57
DucNX <xuanduc987@gmail.com>
2012-09-18 20:59:56 +0700

    THay đổi converstionFunction
    thêm method load ảnh

--- GameBangChuyen/src/com/btl/Model/ConversionFunction.java
+++ GameBangChuyen/src/com/btl/Model/ConversionFunction.java
@@ -1,6 +1,11 @@
 package com.btl.Model;
 
 import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class ConversionFunction {
 	/**
@@ -25,6 +30,15 @@ public class ConversionFunction {
 		Double ux = (double) (Math.sqrt(3) * t.y + t.x) / 2;
 		Double uy = (double) (t.x - Math.sqrt(3) * t.y) / 2;
 		return new Point(ux.intValue(), uy.intValue());
+	}
+
+	public static BufferedImage loadImage(String fileDir) {
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File(fileDir));
+		} catch (IOException e) {
+		}
+		return img;
 	}
 
 }
