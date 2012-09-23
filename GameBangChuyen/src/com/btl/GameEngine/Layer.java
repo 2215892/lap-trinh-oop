d7ff5c5ff49906f08377d2b23607503920ba257f
DucNX <xuanduc987@gmail.com>
2012-09-23 13:23:47 +0700

    Layer

--- GameBangChuyen/src/com/btl/GameEngine/Layer.java
+++ GameBangChuyen/src/com/btl/GameEngine/Layer.java
@@ -1,11 +1,76 @@
 package com.btl.GameEngine;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Layer.
+ */
 public class Layer {
-	Image buffer;
 
+	/** The buffer. */
+	private BufferedImage buffer;
+	public boolean isVisible = true;
+
+	private ArrayList<Drawable> listDrawable;
+
+	public Layer(int width, int height) {
+		buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		listDrawable = new ArrayList<Drawable>();
+	}
+
+	/**
+	 * Lay doi tuong bi click chuot
+	 * 
+	 * @param p
+	 *            Toa do click chuot
+	 * @return Doi tuong bi click, null neu khong co
+	 */
+	public Drawable getClickedObj(Point p) {
+		if (this.isVisible) {
+			for (Drawable i : listDrawable) {
+				if (i.contains(p))
+					return i;
+			}
+		}
+		return null;
+	}
+
+	public void render() {
+		Graphics2D g = (Graphics2D) this.buffer.getGraphics();
+
+		/* Xoa nen buffer */
+		g.setBackground(new Color(255, 255, 255, 0));
+		g.clearRect(0, 0, buffer.getWidth(), buffer.getHeight());
+
+		for (Drawable i : listDrawable) {
+			i.paint(g);
+		}
+	}
+
+	public void show() {
+		this.isVisible = true;
+	}
+
+	public void hide() {
+		this.isVisible = false;
+	}
+
+	/**
+	 * Lay hinh anh cua layer.
+	 * 
+	 * @return the layer
+	 */
 	public Image getLayer() {
-		return buffer;
+		if (this.isVisible)
+			return buffer;
+		else
+			return new BufferedImage(buffer.getWidth(), buffer.getHeight(),
+					BufferedImage.TYPE_INT_ARGB);
 	}
 }
