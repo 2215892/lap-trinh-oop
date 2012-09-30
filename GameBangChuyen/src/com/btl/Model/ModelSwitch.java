8daa47a70571786857f8a1b975398605e8939ce7
DucNX <xuanduc987@gmail.com>
2012-09-30 11:03:30 +0700

    ModelSwitch
    Fix loi

--- GameBangChuyen/src/com/btl/Model/ModelSwitch.java
+++ GameBangChuyen/src/com/btl/Model/ModelSwitch.java
@@ -7,7 +7,7 @@ public abstract class ModelSwitch {
 
 	protected ArrayList<Direction> listDirection;
 	protected Point position;
-	protected int currentDir;
+	protected int currentDir = -1; /* Khong co huong nao thi currentDir = -1 */
 
 	public ModelSwitch(Point p) {
 		this.position = p;
@@ -17,14 +17,27 @@ public abstract class ModelSwitch {
 
 	public void addDirection(Direction d) {
 		listDirection.add(d);
+
+		if (this.listDirection.size() == 1) {
+			this.currentDir = 0;
+		}
 	}
 
 	public void removeDirection(Direction d) {
 		listDirection.remove(d);
+
+		if (this.listDirection.isEmpty()) {
+			this.currentDir = -1;
+		}
 	}
 
 	public Boolean containsDirection(Direction d) {
 		return listDirection.contains(d);
 	}
 
+	public void changeDirection() {
+		if (this.currentDir != -1) {
+			this.currentDir = (this.currentDir + 1) / this.listDirection.size();
+		}
+	}
 }
