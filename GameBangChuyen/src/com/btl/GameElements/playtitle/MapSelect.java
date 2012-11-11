8028953335afc7255325d2db04e636e1068521d8
DucNX <xuanduc987@gmail.com>
2012-11-11 23:50:14 +0700

    MapSelect
    rewrite

--- GameBangChuyen/src/com/btl/GameElements/playtitle/MapSelect.java
+++ GameBangChuyen/src/com/btl/GameElements/playtitle/MapSelect.java
@@ -6,9 +6,13 @@ import java.awt.event.MouseEvent;
 
 import com.btl.GameBoard.GamePanel;
 import com.btl.GameBoard.GameState;
+import com.btl.GameElements.playstate.Button;
 import com.btl.GameElements.playstate.PlayState;
+import com.btl.GameEngine.Layer;
 import com.btl.Model.ConversionFunction;
 import com.btl.Model.ModelMap;
+import com.btl.data.ButtonImage;
+import com.btl.data.OtherImage;
 import com.btl.data.SaveFile;
 import com.btl.data.SoundEffect;
 
@@ -17,17 +21,33 @@ public class MapSelect extends GameState {
 	private SaveFile saveFile = SaveFile.create();
 	public static final int LEVEL_COUNT = 15;
 	private MapButton[] mButtons = new MapButton[LEVEL_COUNT];
+	private Button btnBack;
+	private Layer layer;
 	private boolean needUpdate = false;
 
 	public MapSelect(GamePanel parent, GameState lastState) {
 		super(parent, lastState);
 
-		for (int i = 0; i < LEVEL_COUNT; ++i) {
-			mButtons[i] = new MapButton(new Point(100 + 100 * (i % 5),
-					100 + 100 * (i / 5)), i + 1, saveFile.getLock(i + 1));
-		}
+		initialize();
 	}
 
+	private void initialize() {
+
+		layer = new Layer(parent.width, parent.height);
+		layer.setBackground(OtherImage.BG);
+
+		/* Khoi tao cac button level */
+		for (int i = 0; i < LEVEL_COUNT; ++i) {
+			mButtons[i] = new MapButton(new Point(100 + 100 * (i % 5),
+					70 + 100 * (i / 5)), i + 1, saveFile.getLock(i + 1));
+
+			layer.addDrawable(mButtons[i]);
+		}
+
+		btnBack = new Button(new Point(245, 425));
+		btnBack.setImage(ButtonImage.BTN_BACK);
+		layer.addDrawable(btnBack);
+	}
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
@@ -46,11 +66,17 @@ public class MapSelect extends GameState {
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		Point p = new Point(arg0.getX(), arg0.getY());
-		for (MapButton btn : mButtons) {
-			if (btn != null && btn.contains(p)) {
+		Button clicked = (Button) layer.getClickedObj(new Point(arg0.getX(),
+				arg0.getY()));
+
+		if (clicked != null) {
+			SoundEffect.BUTTONCLICK.play();
+			if (clicked == btnBack) {
+				changeState(lastState);
+			} else {
+				MapButton btn = (MapButton) clicked;
+
 				int id = btn.getId();
-				SoundEffect.BUTTONCLICK.play();
 				if (!saveFile.getLock(id)) {
 					ModelMap map = ModelMap.createMap(ConversionFunction
 							.getCurrentDirectory() + "map//" + id);
@@ -64,10 +90,7 @@ public class MapSelect extends GameState {
 
 						parent.setState(playState);
 					}
-
 				}
-
-				break;
 			}
 		}
 
@@ -80,16 +103,19 @@ public class MapSelect extends GameState {
 
 	@Override
 	public void gameRender(Graphics g) {
-		for (MapButton btn : mButtons) {
-			if (btn != null) {
-				if (needUpdate)
+
+		if (needUpdate) {
+			for (MapButton btn : mButtons) {
+				if (btn != null) {
 					btn.update();
-				btn.paint(g);
+				}
 			}
 		}
 
-	}
+		layer.render();
+		g.drawImage(layer.getLayer(), 0, 0, null);
 
+	}
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
