3195a3a34ff7b0bbd3a1bf3144fbd629928e71ad
DucNX <xuanduc987@gmail.com>
2012-11-12 00:17:10 +0700

    PlayState
    sua lai chut phan viet thong tin

--- GameBangChuyen/src/com/btl/GameElements/playstate/PlayState.java
+++ GameBangChuyen/src/com/btl/GameElements/playstate/PlayState.java
@@ -1,8 +1,11 @@
 package com.btl.GameElements.playstate;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
@@ -146,14 +149,30 @@ public class PlayState extends GameState {
 
 		/* Ve len man hinh */
 		g.drawImage(this.buffer, 0, 0, null);
-		g.drawString("Time: " + secondToString(currentSecond) + " Score: "
-				+ Integer.toString(score), 10, 10);
+
+		/* Viet chu, thong tin len man hinh */
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		Font font = new Font("TimesRoman", Font.PLAIN, 16);
+		g2.setFont(font);
+		g2.drawString(" Score: " + Integer.toString(score) + "         Time: "
+				+ secondToString(currentSecond), 10, 20);
 		if (highscore != -1) {
-			g.drawString(" High Score: " + Integer.toString(highscore), 200, 10);
+			g2.drawString(" High Score: ", 350, 20);
+			font = new Font("TimesRoman", Font.BOLD, 18);
+			g2.setFont(font);
+			if (highscore >= score) {
+				g2.setColor(Color.GRAY);
+				g2.drawString(Integer.toString(highscore), 450, 20);
+			} else {
+				g2.setColor(Color.GREEN);
+				g2.drawString(Integer.toString(score), 450, 20);
+			}
+
 		}
 
 	}
-
 	private String secondToString(int second) {
 
 		int m = second / 60;
