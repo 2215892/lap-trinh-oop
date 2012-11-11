c3ceca5270408f91a215f23df13f9b5ab8caf6bf
DucNX <xuanduc987@gmail.com>
2012-11-11 20:02:52 +0700

    GamePanel
    resize

--- GameBangChuyen/src/com/btl/GameBoard/GamePanel.java
+++ GameBangChuyen/src/com/btl/GameBoard/GamePanel.java
@@ -13,8 +13,8 @@ import javax.swing.SwingUtilities;
 public class GamePanel extends JPanel {
 
 	public static final int WIDTH = 1;
-	public int width = 700;
-	public int height = 500;
+	public int width = 685;
+	public int height = 485;
 
 	/**
 	 * 
